05882e0c527302f975e713a0aef99585a044641f
JoaoVictor <jvsn.10.jv@gmail.com>
2025-07-08 19:51:20 -0300

    Refactor file structure and improve readability in DEV06P10 program and fix minor compilation errors.

--- DEVxxP10.cbl
+++ DEVxxP10.cbl
@@ -1,5 +1,7 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. DEV06P10.
+      *
+      *
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
@@ -7,27 +9,35 @@
                FILE STATUS IS WS-FS-DADOSCLI.
            SELECT ARQ-RLINCONS ASSIGN TO 'RLINCONS'
                FILE STATUS IS WS-FS-RLINCONS.
+      *
+      *
        DATA DIVISION.
+      *
        FILE SECTION.
+      *
        FD ARQ-DADOSCLI
            RECORDING MODE F
            RECORD CONTAINS 80 CHARACTERS
            BLOCK CONTAINS 0 RECORDS.
        01 REG-DADOSCLI             PIC X(80).
+      * 
        FD ARQ-RLINCONS
            RECORDING MODE F
-           RECORD CONTAINS 132 CHARACTERS.
+           RECORD CONTAINS 132 CHARACTERS. 
        01 REG-RLINCONS             PIC X(132).
+      * 
        WORKING-STORAGE SECTION.
+      * 
        01 WS-CLIENTE-HOST.
-          05 WS-CODIGOCLI-HOST         PIC 9(9).
-          05 WS-RAZAOSOCIAL-HOST       PIC X(60).
-          05 WS-CNPJ-HOST              PIC X(8).
-          05 WS-FILIAL-HOST            PIC X(4).
-          05 WS-CONTROLE-HOST          PIC 9(2).
-          05 WS-VLRULTCOMPRA-HOST      PIC S9(9)V99.
-          05 WS-DATAOPER-HOST          PIC 9(8).
-          05 WS-DATA-PROCESSAMENTO-HOST PIC 9(8).
+           05 WS-CODIGOCLI-HOST         PIC 9(9).
+           05 WS-RAZAOSOCIAL-HOST       PIC X(60).
+           05 WS-CNPJ-HOST              PIC X(8).
+           05 WS-FILIAL-HOST            PIC X(4).
+           05 WS-CONTROLE-HOST          PIC 9(2).
+           05 WS-VLRULTCOMPRA-HOST      PIC S9(9)V99.
+           05 WS-DATAOPER-HOST          PIC 9(8).
+           05 WS-DATA-PROCESSAMENTO-HOST PIC 9(8).
+      *
        01 WS-FS-DADOSCLI           PIC XX VALUE SPACES.
        01 WS-FS-RLINCONS           PIC XX VALUE SPACES.
        01 WS-CONTROLE.
@@ -35,45 +45,70 @@
            05 WS-REG-ATUAL         PIC 9(5) VALUE ZEROS.
            05 WS-REG-DESP          PIC 9(5) VALUE ZEROS.
        01 WS-FIM-ARQUIVO           PIC X VALUE 'N'.
-       COPY DEVBKCLI.
+      * 
+      * COPY DEVBKCLI.
+      * Descomente o copy acima e comente o abaixo para usar o copybook
+       01 PRF-DADOSCLI.
+            10 PRF-OPERACAO            PIC X(001)    VALUE SPACE.
+               88 NOVO-CLIENTE                       VALUE 'N'.
+               88 ATUALIZACAO                        VALUE 'A'.
+               88 INATIVACAO                         VALUE 'I'.
+            10 PRF-CODIGOCLI           PIC 9(005)    VALUE ZEROS.
+            10 PRF-RAZAOSOCIAL         PIC X(040)    VALUE SPACE.
+            10 PRF-CNPJ                PIC X(009)    VALUE SPACE.
+            10 PRF-FILIAL              PIC X(004)    VALUE SPACE.
+            10 PRF-CONTROLE            PIC 9(002)    VALUE ZEROS.
+            10 PRF-VLRULTCOMPRA COMP-3 PIC 9(011)V99 VALUE ZEROS.
+            10 PRF-DATAOPER            PIC 9(008)    VALUE ZEROS.
+            10 PRF-RESERVA             PIC X(004)    VALUE SPACE.
+      * 
        01 WRK-DEVCDATA.
            05 WRK-DATADEV          PIC 9(8) VALUE ZEROS.
            05 WRK-CODRDEV          PIC X(2) VALUE SPACES.
+      * 
        01 WRK-AREACNPJ.
            05 WRK-DADOS-CNPJ.
                10 WRK-CNPJ         PIC X(8).
                10 WRK-FILIAL       PIC X(4).
                10 WRK-CONTROLE     PIC 9(2).
+      * 
        01 WRK-DATA-PROCESSAMENTO   PIC 9(8).
        01 WRK-HORA-PROCESSAMENTO   PIC 9(6).
        01 WRK-PROG-CNPJ            PIC X(8) VALUE 'DEV06CPJ'.
        01 WRK-PROG-DAT             PIC X(8) VALUE 'DEVCDATA'.
+      * 
        PROCEDURE DIVISION.
+      * 
        0000-INICIO.
            DISPLAY 'DEV06P10   INICIO DO PROCESSAMENTO'.
            ACCEPT WRK-DATA-PROCESSAMENTO FROM DATE YYYYMMDD.
            ACCEPT WRK-HORA-PROCESSAMENTO FROM TIME.
            OPEN INPUT ARQ-DADOSCLI
                 OUTPUT ARQ-RLINCONS.
+      * 
            IF WS-FS-DADOSCLI NOT = '00'
                DISPLAY 'DEV06P10   ERRO ABRIR DADOSCLI'
                PERFORM 9999-FIM
                STOP RUN
            END-IF
+      * 
            IF WS-FS-RLINCONS NOT = '00'
                DISPLAY 'DEV06P10   ERRO ABRIR RLINCONS'
                PERFORM 9999-FIM
                STOP RUN
            END-IF
+      * 
            READ ARQ-DADOSCLI INTO PRF-DADOSCLI
                AT END MOVE 'S' TO WS-FIM-ARQUIVO
            END-READ
+      * 
            IF WS-FIM-ARQUIVO = 'S'
                DISPLAY 'DEV06P10   ARQUIVO DADOSCLI VAZIO'
                MOVE 4 TO RETURN-CODE
                PERFORM 9999-FIM
                STOP RUN
            END-IF
+      * 
            PERFORM UNTIL WS-FIM-ARQUIVO = 'S'
                ADD 1 TO WS-REG-LIDOS
                PERFORM 1000-TRATA-REGISTRO
@@ -81,12 +116,16 @@
                    AT END MOVE 'S' TO WS-FIM-ARQUIVO
                END-READ
            END-PERFORM
-           DISPLAY 'DEV06P10-TOTAL DE REGISTROS LIDOS......: ' WS-REG-LIDOS
-           DISPLAY 'DEV06P10-TOTAL DE REGISTROS ATUALIZADOS: ' WS-REG-ATUAL
-           DISPLAY 'DEV06P10-TOTAL DE REGISTROS DESPREZADOS: ' WS-REG-DESP
+      * 
+           DISPLAY 'DEV06P10-TOTAL DE REGISTROS LIDOS......: ' 
+               WS-REG-LIDOS
+           DISPLAY 'DEV06P10-TOTAL DE REGISTROS ATUALIZADOS: ' 
+               WS-REG-ATUAL
+           DISPLAY 'DEV06P10-TOTAL DE REGISTROS DESPREZADOS: '
+               WS-REG-DESP
            DISPLAY 'DEV06P10-PROCESSAMENTO ENCERRADO'
            PERFORM 9999-FIM.
-
+      *     
        1000-TRATA-REGISTRO.
            EVALUATE TRUE
              WHEN NOVO-CLIENTE
@@ -136,7 +175,7 @@
               PERFORM 8000-REG-INVALIDO
            END-IF
 
-           *--- MOVENDO DADOS PARA VARIAVEIS HOST ---
+      *--- MOVENDO DADOS PARA VARIAVEIS HOST ---
            MOVE PRF-CODIGOCLI       TO WS-CODIGOCLI-HOST
            MOVE PRF-RAZAOSOCIAL     TO WS-RAZAOSOCIAL-HOST
            MOVE PRF-CNPJ            TO WS-CNPJ-HOST
